4e41e0b82f78a3b5cd8b22a59731954117127c09
Giuliano Gioia <giulianogioia86@gmail.com>
2024-03-29 10:50:08 +0100

    add components
    add all needed components, build basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FooterComponent, HeaderComponent, HomeComponent,AboutComponent, SkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
