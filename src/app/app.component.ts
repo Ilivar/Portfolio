5937218e469ffd45f506504892332ed6fe3634b1
Giuliano Gioia <giulianogioia86@gmail.com>
2024-04-05 14:56:53 +0200

    change structure
    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,9 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { SkillsComponent } from '../skills/skills.component';
-import { PortfolioComponent } from '../portfolio/portfolio.component';
-import { ContactComponent } from '../contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
